4c723bc472894b23559285af5be85e2d52e0125c
Rubi Henjaya <rubi.henjaya@gmail.com>
2017-10-04 14:36:08 +0700

    add work

--- client/components.js
+++ client/components.js
@@ -19,9 +19,29 @@ var CommmentForm = React.createClass({
   handleTextChange: function(e){
     this.setState({text: e.target.value})
   },
+  handleSubmit: function(e){
+    e.preventDefault()
+    var author = this.state.author.trim()
+    var text = this.state.text.trim()
+    if(!text || !author){
+      return;
+    }
+    $.ajax({
+      url: "http://localhost:3000/api/comments",
+      dataType: "json",
+      type: "POST",
+      data: {author: author, text: text},
+      success: function(response){
+        alert("berhasil");
+      },
+      error: function(xhr, status, err){
+        alert("gagal");
+      }
+    })
+  },
   render: function(){
     return(
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <input type="text" placeholder="your name" value={this.state.author} onChange={this.handleAuthorChange}/>
         <input type="text" placeholder="say something..." value={this.state.text} onChange={this.handleTextChange}/>
         <input type="submit" value="Send"/>
