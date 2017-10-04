280a893f3397748be5c93e5e3d3795cefb4373bc
Rubi Henjaya <rubi.henjaya@gmail.com>
2017-10-04 14:54:16 +0700

    list done

--- client/components.js
+++ client/components.js
@@ -1,8 +1,28 @@
 var CommentBox = React.createClass({
+  getInitialState: function(){
+    return {data: []}
+  },
+  loadComments: function(){
+    $.ajax({
+      url: "http://localhost:3000/api/comments",
+      dataType: "json",
+      cache: false,
+      success: function(response){
+        this.setState({data: response})
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.error(status, err.toString());
+      }
+    })
+  },
+  componentDidMount: function(){
+    this.loadComments();
+  },
   render: function(){
     return(
       <div className="commentBox">
         <h1>Comments App</h1>
+        <CommentList data={this.state.data}/>
         <CommmentForm/>
       </div>
     )
@@ -30,7 +50,7 @@ var CommmentForm = React.createClass({
       url: "http://localhost:3000/api/comments",
       dataType: "json",
       type: "POST",
-      data: {author: author, text: text},
+      data: {id: Date.now(), author: author, text: text},
       success: function(response){
         alert("berhasil");
       },
@@ -46,6 +66,30 @@ var CommmentForm = React.createClass({
         <input type="text" placeholder="say something..." value={this.state.text} onChange={this.handleTextChange}/>
         <input type="submit" value="Send"/>
       </form>
+    )
+  }
+})
+
+var Comment = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h4>{this.props.author}</h4>
+        <p>{this.props.text}</p>
+      </div>
+    )
+  }
+})
+
+var CommentList = React.createClass({
+  render: function(){
+    var commentNodes = this.props.data.map(function(comment){
+      return(<Comment key={comment.id} author={comment.author} text={comment.text}/>)
+    })
+    return(
+      <div>
+      {commentNodes}
+      </div>
     )
   }
 })
